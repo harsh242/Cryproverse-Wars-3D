8c39596f8f8e9f62717ff8ca952b275d25e5dad3
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2020-10-05 00:11:28 +0530

    color customizer for one body part added

--- src/components/model.js
+++ src/components/model.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import * as THREE from "three";
 import GLTFLoader from "three-gltf-loader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import robot from "./scene.gltf";
+import robot from "./robot.glb";
 
 class Model extends Component {
   componentDidMount() {
@@ -11,7 +11,8 @@ class Model extends Component {
     this.scene = new THREE.Scene();
     //Add Renderer
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.renderer.setClearColor("#263238");
+
+    this.renderer.setClearColor("#f1f1f1");
     this.renderer.setSize(width, height);
     this.mount.appendChild(this.renderer.domElement);
 
@@ -21,27 +22,126 @@ class Model extends Component {
     this.camera.position.y = 5;
 
     //Camera Controls
-    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+    var controls = new OrbitControls(this.camera, this.renderer.domElement);
 
     //LIGHTS
-    var lights = [];
-    lights[0] = new THREE.PointLight(0x304ffe, 1, 0);
-    lights[1] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[2] = new THREE.PointLight(0xffffff, 1, 0);
-    lights[0].position.set(0, 200, 0);
-    lights[1].position.set(100, 200, 100);
-    lights[2].position.set(-100, -200, -100);
-    this.scene.add(lights[0]);
-    this.scene.add(lights[1]);
-    this.scene.add(lights[2]);
-    //ADD Your 3D Models here
+    var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+    hemiLight.position.set(0, 50, 0);
+    // Add hemisphere light to scene
+    this.scene.add(hemiLight);
 
+    var dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+    dirLight.position.set(-8, 12, 8);
+    dirLight.castShadow = true;
+    dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+    // Add directional Light to scene
+    this.scene.add(dirLight);
+
+    // Floor
+    var floorGeometry = new THREE.PlaneGeometry(5000, 5000, 1, 1);
+    var floorMaterial = new THREE.MeshPhongMaterial({
+      color: 0xeeeeee,
+      shininess: 0,
+    });
+
+    var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = -0.5 * Math.PI;
+    floor.receiveShadow = true;
+    floor.position.y = -1;
+    this.scene.add(floor);
+
+    //External 3D Model Code
+    const colors = [
+      {
+        color: "66533C",
+      },
+      {
+        color: "173A2F",
+      },
+      {
+        color: "153944",
+      },
+      {
+        color: "27548D",
+      },
+      {
+        color: "438AAC",
+      },
+    ];
+
+    const TRAY = document.getElementById("js-tray-slide");
+
+    // Function - Build Colors
+    function buildColors(colors) {
+      for (let [i, color] of colors.entries()) {
+        let swatch = document.createElement("div");
+        swatch.classList.add("tray__swatch");
+
+        swatch.style.background = "#" + color.color;
+
+        swatch.setAttribute("data-key", i);
+        TRAY.append(swatch);
+      }
+    }
+
+    buildColors(colors);
+
+    // Swatches
+    const swatches = document.querySelectorAll(".tray__swatch");
+
+    for (const swatch of swatches) {
+      swatch.addEventListener("click", selectSwatch);
+    }
+
+    function selectSwatch(e) {
+      let color = colors[parseInt(e.target.dataset.key)];
+      let new_mtl;
+
+      new_mtl = new THREE.MeshPhongMaterial({
+        color: parseInt("0x" + color.color),
+        shininess: color.shininess ? color.shininess : 10,
+      });
+
+      setMaterial(theModel, "Body", new_mtl);
+    }
+
+    function setMaterial(parent, type, mtl) {
+      parent.traverse((o) => {
+        if (o.isMesh && o.nameID != null) {
+          if (o.nameID == type) {
+            o.material = mtl;
+          }
+        }
+      });
+    }
+
+    // Initial material
+    const INITIAL_MTL = new THREE.MeshPhongMaterial({
+      color: 0xf1f1f1,
+      shininess: 10,
+    });
+
+    const INITIAL_MAP = [
+      { childID: "Body", mtl: INITIAL_MTL },
+      { childID: "Leg_Left", mtl: INITIAL_MTL },
+      { childID: "Leg_Right", mtl: INITIAL_MTL },
+      { childID: "Neck", mtl: INITIAL_MTL },
+      { childID: "Shoulders", mtl: INITIAL_MTL },
+    ];
+
+    var theModel;
     const loader = new GLTFLoader();
     loader.load(
-      "scene.gltf",
+      robot,
       (gltf) => {
         // called when the resource is loaded
-        this.scene.add(gltf.scene);
+        theModel = gltf.scene;
+        // theModel.scale.set(0.5, 0.5, 0.5);
+        for (let object of INITIAL_MAP) {
+          initColor(theModel, object.childID, object.mtl);
+        }
+
+        this.scene.add(theModel);
       },
       (xhr) => {
         // called while loading is progressing
@@ -52,6 +152,18 @@ class Model extends Component {
         console.error("An error happened", error);
       }
     );
+
+    // Function - Add the textures to the models
+    function initColor(parent, type, mtl) {
+      parent.traverse((o) => {
+        if (o.isMesh) {
+          if (o.name.includes(type)) {
+            o.material = mtl;
+            o.nameID = type; // Set a new property to identify this object
+          }
+        }
+      });
+    }
 
     this.renderScene();
     //start animation
@@ -82,12 +194,19 @@ class Model extends Component {
 
   render() {
     return (
-      <div
-        style={{ width: "800px", height: "800px" }}
-        ref={(mount) => {
-          this.mount = mount;
-        }}
-      />
+      <div>
+        <div
+          style={{ width: "800px", height: "800px" }}
+          ref={(mount) => {
+            this.mount = mount;
+          }}
+        />
+        <div class="controls">
+          <div id="js-tray" class="tray">
+            <div id="js-tray-slide" class="tray__slide"></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
