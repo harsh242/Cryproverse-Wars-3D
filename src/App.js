51c0a9890715926068dd812e871ab23dfa3a4111
harshbadhai242 <49694914+harsh242@users.noreply.github.com>
2020-11-25 20:39:32 +0530

    :wastebucket: created new repo for react-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+<<<<<<< HEAD
 import Yellow from "./components/model";
+=======
+import Model from "./components/model";
+>>>>>>> parent of 9af42b4... badass 3d model added
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
       <Yellow />
+=======
+      <Model />
+>>>>>>> parent of 9af42b4... badass 3d model added
     </div>
   );
 }
